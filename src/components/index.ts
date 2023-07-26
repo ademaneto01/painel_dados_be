a9cae2f280cc5f1f445d96b1b16590d2e05b2c38
Ademar Neto <ademarneto47@gmail.com>
2023-07-26 17:13:54 -0300

    erros page/main

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import SideNavBar from './sideNavBar';
-import * as pages from './pages';
+import * as pages from './pages/index';
 import ErrorComponent from './ErrorComponent';
 
 export { SideNavBar, pages, ErrorComponent };
