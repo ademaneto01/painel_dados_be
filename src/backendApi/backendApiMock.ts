56704679b194c1cab1ae89f1766e14aa93b272be
Ademar Neto <ademar@beducation.com.br>
2023-08-14 15:46:19 -0300

    testando outra porta

--- src/backendApi/backendApiMock.ts
+++ src/backendApi/backendApiMock.ts
@@ -25,7 +25,7 @@ export default class BackendApiMock implements BackendApiInterface {
   constructor(accessToken?: string) {
     this.accessToken = accessToken || undefined;
     this.api = axios.create({
-      baseURL: 'http://45.231.135.54:3001',
+      baseURL: 'http://45.231.135.54:3000',
       timeout: 30000,
       headers: {
         'Content-Type': 'application/json',
