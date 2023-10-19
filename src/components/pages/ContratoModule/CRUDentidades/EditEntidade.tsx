0513c159911359f6602a74c69fe364c55df692f1
Ademar Neto <ademar@beducation.com.br>
2023-10-19 14:53:01 -0300

    implementando validações de CNPJ e CEP

--- src/components/pages/ContratoModule/CRUDentidades/EditEntidade.tsx
+++ src/components/pages/ContratoModule/CRUDentidades/EditEntidade.tsx
@@ -1,4 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import InputMask from 'react-input-mask';
 import styles from '@/styles/NovoContrato.module.css';
 import { FailedToFetchError } from '@/errors';
 import BackendApiMock from '@/backendApi';
@@ -7,6 +8,7 @@ import { PageEnumContratos } from '@/enums';
 import { PageContentContainer, CreateButton } from '@/components/shared';
 import { useGlobalContext } from '@/context/store';
 import { EntitiesUsuariosPDG } from '@/entities';
+import validaCNPJ from '@/validations/validaCNPJ';
 
 interface FormData {
   id: string | null;
@@ -131,11 +133,38 @@ export default function EditEntidadeEscolar(): JSX.Element {
     setPage(PageEnumContratos.entidadesEscolares);
   };
 
+  const fetchEndereco = async (cep: string) => {
+    try {
+      cep = cep.replace(/-/g, '');
+      if (cep.length !== 8) {
+        return null;
+      }
+      if (cep.length === 8) {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+
+        if (!data.erro) {
+          setFormData((prev) => ({
+            ...prev,
+            endereco: data.logradouro,
+            cidade: data.localidade,
+            uf: data.uf,
+            bairro: data.bairro,
+            complemento: data.complemento,
+          }));
+        } else {
+          setError(true);
+          setMsgError('CEP não encontrato...');
+          setTimeout(() => setError(false), 5000);
+        }
+      }
+    } catch (error) {
+      console.error('Erro ao buscar o CEP:', error);
+    }
+  };
+
   const validateForm = (): boolean => {
     const errors: string[] = [];
-    // if (Object.values(formData).some((v) => v === '' || v === null)) {
-    //   errors.push('Todos campos são obrigatórios...');
-    // }
 
     for (const [key, value] of Object.entries(formData)) {
       if (key !== 'url_dados' && (value === '' || value === null)) {
@@ -145,6 +174,11 @@ export default function EditEntidadeEscolar(): JSX.Element {
     }
     if (formData.uf && formData.uf.length > 2) {
       errors.push('Campo UF é permitido somente dois caracteres...');
+    }
+    if (formData.cnpj_escola) {
+      if (!validaCNPJ(formData.cnpj_escola)) {
+        errors.push('CNPJ inválido...');
+      }
     }
     if (errors.length) {
       setError(true);
@@ -159,6 +193,9 @@ export default function EditEntidadeEscolar(): JSX.Element {
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
     const { name, value } = e.target;
+    if (name === 'cep') {
+      fetchEndereco(value);
+    }
     if (name === 'bo_rede') {
       const booleanValue =
         value === 'true' ? true : value === 'false' ? false : null;
@@ -245,9 +282,10 @@ const FormComponent: React.FC<any> = ({
       </label>
       <label className={styles.labelStandard}>
         CNPJ Escola
-        <input
+        <InputMask
           type="text"
           placeholder="CNPJ Escola"
+          mask="99.999.999/9999-99"
           name="cnpj_escola"
           value={formData.cnpj_escola ?? ''}
           onChange={handleInputChange}
@@ -256,9 +294,10 @@ const FormComponent: React.FC<any> = ({
       </label>
       <label className={styles.labelStandard}>
         CEP
-        <input
+        <InputMask
           type="text"
           placeholder="CEP"
+          mask="99999-999"
           name="cep"
           value={formData.cep ?? ''}
           onChange={handleInputChange}
--- src/components/pages/ContratoModule/CRUDentidades/NovaEntidade.tsx
+++ src/components/pages/ContratoModule/CRUDentidades/NovaEntidade.tsx
@@ -1,4 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import InputMask from 'react-input-mask';
 import styles from '@/styles/NovoContrato.module.css';
 import { FailedToFetchError } from '@/errors';
 import BackendApiMock from '@/backendApi';
@@ -7,6 +8,7 @@ import { PageEnumContratos } from '@/enums';
 import { PageContentContainer, CreateButton } from '@/components/shared';
 import { useGlobalContext } from '@/context/store';
 import { EntitiesUsuariosPDG } from '@/entities';
+import validaCNPJ from '@/validations/validaCNPJ';
 
 interface FormData {
   nome_operacional: string;
@@ -86,10 +88,43 @@ export default function NovaEntidade(): JSX.Element {
     }
   };
 
+  const fetchEndereco = async (cep: string) => {
+    try {
+      cep = cep.replace(/-/g, '');
+      if (cep.length !== 8) {
+        return null;
+      }
+      if (cep.length === 8) {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+
+        if (!data.erro) {
+          setFormData((prev) => ({
+            ...prev,
+            endereco: data.logradouro,
+            cidade: data.localidade,
+            uf: data.uf,
+            bairro: data.bairro,
+            complemento: data.complemento,
+          }));
+        } else {
+          setError(true);
+          setMsgError('CEP não encontrato...');
+          setTimeout(() => setError(false), 5000);
+        }
+      }
+    } catch (error) {
+      console.error('Erro ao buscar o CEP:', error);
+    }
+  };
+
   const handleInputChange = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
     const { name, value } = e.target;
+    if (name === 'cep') {
+      fetchEndereco(value);
+    }
     const booleanValue =
       value === 'true' ? true : value === 'false' ? false : null;
     const updatedValue = ['ativo'].includes(name) ? booleanValue : value;
@@ -113,6 +148,9 @@ export default function NovaEntidade(): JSX.Element {
     }
     if (formData.uf.length > 2) {
       errors.push('Campo UF é permitido somente dois caracteres...');
+    }
+    if (!validaCNPJ(formData.cnpj_escola)) {
+      errors.push('CNPJ inválido...');
     }
     if (errors.length) {
       setError(true);
@@ -202,9 +240,10 @@ const FormComponent: React.FC<any> = ({
       </label>
       <label className={styles.labelStandard}>
         CNPJ Escola
-        <input
+        <InputMask
           type="text"
           placeholder="CNPJ Escola"
+          mask="99.999.999/9999-99"
           name="cnpj_escola"
           value={formData.cnpj_escola}
           onChange={handleInputChange}
@@ -213,9 +252,10 @@ const FormComponent: React.FC<any> = ({
       </label>
       <label className={styles.labelStandard}>
         CEP
-        <input
+        <InputMask
           type="text"
           placeholder="CEP"
+          mask="99999-999"
           name="cep"
           value={formData.cep}
           onChange={handleInputChange}
