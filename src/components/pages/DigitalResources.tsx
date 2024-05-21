7ec4298868097436b15aab800a8127670133f8e4
Ademar <ademarneto47@gmail.com>
2024-05-21 17:31:11 -0300

    fix: harcode urls

--- src/components/pages/DigitalResources.tsx
+++ src/components/pages/DigitalResources.tsx
@@ -29,54 +29,29 @@ export default function DigitalResources() {
           const nomeEscola = dadosEntidade[0].nome_operacional;
 
           switch (nomeEscola) {
-            case 'Centro Educacional Da Lagoa - Unidade Barra':
+            case 'Rede Batista de Educação GERAL':
               setIframe(
-                'https://dados.be.education/public/dashboard/7ee9fc27-1752-4cad-a673-01322b99b999',
+                'https://dados.be.education/public/dashboard/df703a86-843c-4c06-a811-a5661096127e',
               );
               break;
-            case 'Centro Educacional Da Lagoa - Unidade Lopes Quintas':
+            case 'COC Santa Catarina GERAL':
               setIframe(
-                'https://dados.be.education/public/dashboard/5138a9b9-fc3d-40ed-b331-6ffe5b855879',
+                'https://dados.be.education/public/dashboard/f0978c17-a567-4586-a7ee-26758e7b31a9',
               );
               break;
-            case 'Centro Educacional Da Lagoa - Unidade Norte Shopping':
+            case 'Liessin GERAL':
               setIframe(
-                'https://dados.be.education/public/dashboard/59398558-3a1b-462d-9359-967bd05d7df6',
+                'https://dados.be.education/public/dashboard/039f0824-36ae-428f-89b4-a5af3ad48691',
               );
               break;
-            case 'Colegio Batista Mineiro - Unidade Floresta SF':
+            case 'Notre Dame - GERAL':
               setIframe(
-                'https://dados.be.education/public/dashboard/51ea5c8b-de42-4638-b575-cb07d434530b',
+                'https://dados.be.education/public/dashboard/c5301435-e59c-49f5-b7fd-5d382c2a5599',
               );
               break;
-            case 'Colegio Batista Mineiro - Unidade Floresta SI':
+            case 'Razão GERAL':
               setIframe(
-                'https://dados.be.education/public/dashboard/876bf592-92d9-4832-8a31-80d4b6966f05',
-              );
-              break;
-            case 'Interamérica - Unidade 1':
-              setIframe(
-                'https://dados.be.education/public/dashboard/90f1b181-98a4-4fee-ac27-da943bf75d72',
-              );
-              break;
-            case 'Interamérica - Unidade 2':
-              setIframe(
-                'https://dados.be.education/public/dashboard/85bd149a-001b-4d3f-a30d-e4d4ed68ed47',
-              );
-              break;
-            case 'Liessin - Unidade Barra da Tijuca':
-              setIframe(
-                'https://dados.be.education/public/dashboard/4752d95b-6802-4259-8868-2e439ebcf067',
-              );
-              break;
-            case 'Liessin - Unidade Botafogo':
-              setIframe(
-                'https://dados.be.education/public/dashboard/7a3163dd-f964-4109-89c9-5a46f814c085',
-              );
-              break;
-            case 'Liessin - Unidade Leblon':
-              setIframe(
-                'https://dados.be.education/public/dashboard/c02797af-41a9-4194-84fb-c9ff12cbd8f1',
+                'https://dados.be.education/public/dashboard/62a369f7-77fe-4880-8280-fc8705d08adc',
               );
               break;
 
@@ -88,10 +63,6 @@ export default function DigitalResources() {
           setNaoContemDados(true);
           return;
         }
-
-        // if (!iframe) {
-        //   setNaoContemDados(true);
-        // }
       } catch (error: any) {
         handleApiErrors(error, setError, setMsgError);
       } finally {
